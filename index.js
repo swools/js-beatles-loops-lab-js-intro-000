63c3613feaf39049fb35bc966bd112549001a0d2
Stephen Wool <stevewool7@gmail.com>
2019-03-05 00:13:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = []
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray;
+}
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i <= facts.length) {
+    facts[i] = facts[i] + `!!!`;
+    i++;
+  }
+}
